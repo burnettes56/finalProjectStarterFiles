955e0cda7e6cc25c4cfb3ccfcdddd6bdf21760b9
haydenBurnette <burnettes@etsu.edu>
2018-04-25 16:43:07 -0400

    Push Commits

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
+/*
+    Author :
+    File   : main.js
+    Date   :
+    Mod    :
+    Purpose: The purpose of this to demonstrate
+        how javaScript, jQuery,
+        and animations inside
+        of CSS3 can be used to
+        make unique elements
+        for a webpages.
+
+*/
 $(document).ready(function () {
 
 });
